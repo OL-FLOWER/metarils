739a4b4456ed99bc5eb5f8919792a7a56a133684
unknown <pitbullali777@gmail.com>
2020-05-09 22:02:22 +0300

    fix signup

--- db/students.inc.php
+++ db/students.inc.php
@@ -10,7 +10,7 @@ class Studens extends DB
     function addStudent($data){
         $connection = $this->connection();
         $q = "insert into {$this->table} (fname,mname,lname,stage,dob,user_id) values ('".$data['fname']."','".$data['mname']."','".$data['lname']."','".$data['stage']."','".$data['dob']."',".$data['user_id'].") ";
-
+        $query = mysqli_query($connection,$q);
         $data['id'] = mysqli_insert_id($connection);
         return $data;
     }
